4d6cebef7fbbdd0e3ed0b8c2957f7775497b20cc
Oliver Kowalke <oliver.kowalke@gmail.com>
2018-07-16 21:35:32 +0200

    IMDB (evaluating pretrained GloVe word embedding)

---
+++ imdb_glove_embedding_eval.R
@@ -0,0 +1,48 @@
+library(keras)
+
+imdb_dir <- "~/Downloads/aclImdb"
+test_dir <- file.path(imdb_dir, "test")
+
+maxlen <- 100 # cut of the text after 100 words
+max_words <- 10000 # consider only the to 10000 words in the data set
+
+embedding_dim <- 100
+
+labels <- c()
+texts <- c()
+
+for (label_type in c("neg", "pos")) {
+    label <- switch(label_type, neg=0, pos=1)
+    dir_name <- file.path(test_dir, label_type)
+    for (fname in list.files(dir_name, pattern=glob2rx("*.txt"), full.names=TRUE)) {
+        texts <- c(texts, readChar(fname, file.info(fname)$size))
+        labels <- c(labels, label)
+    }
+}
+
+tokenizer <- text_tokenizer(num_words=max_words) %>%
+    fit_text_tokenizer(texts)
+
+sequences <- texts_to_sequences(tokenizer, texts)
+x_test <- pad_sequences(sequences, maxlen=maxlen)
+y_test <- as.array(labels)
+
+# model
+model <- keras_model_sequential() %>%
+    layer_embedding(input_dim=max_words, output_dim=embedding_dim, input_length=maxlen) %>% #max input length to the embedding layer
+    layer_flatten() %>% # flatten the 3D tensor of embeddings into a 2D tensor of shape (samples, maxlen)
+    layer_dense(units=32, activation="relu") %>%
+    layer_dense(units=1, activation="sigmoid") # classifier on top
+
+# training and evaluation
+model %>% compile(
+    optimizer="rmsprop",
+    loss="binary_crossentropy",
+    metrics=c("accuracy")
+)
+
+# evaluate
+results <- model %>%
+    load_model_weights_hdf5("data/pre_trained_glove_model.h5") %>%
+    evaluate(x_test, y_test)
+results
